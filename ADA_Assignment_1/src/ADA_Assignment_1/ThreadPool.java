689b6e66606b628b771b502489c67f7d5f141c7a
Jack-Briggs <jackbriggs1992@gmail.com>
2022-08-07 21:51:57 +1200

    Everything implemented except resize,
    Testing still needs to be done

--- ADA_Assignment_1/src/ADA_Assignment_1/ThreadPool.java
+++ ADA_Assignment_1/src/ADA_Assignment_1/ThreadPool.java
@@ -1,6 +1,8 @@
 package ADA_Assignment_1;
 
 import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -10,34 +12,74 @@ public class ThreadPool {
     
     static final int INITIAL_CAPACITY = 10;
     static final double LOAD_FACTOR = 0.70f;
+    private boolean destroyPool = false;
     
     // Data structures for the task queue and threads
-    private LinkedList taskQueue;
+    private LinkedList<Task> taskQueue;
     private Thread[] threads;
-    // Array for holding if a thread is performing a task or not;
-    private boolean[] performing;
     
     // Create a new ThreadPool with a default size of INITIAL_CAPACITY;
     public ThreadPool() {
         taskQueue = new LinkedList();
         threads = new Thread[INITIAL_CAPACITY];
+        createThreads();
     }
     
     // Create a new ThreadPool with a customizable default size;
     public ThreadPool(int initialCapacity) {
         taskQueue = new LinkedList();
         threads = new Thread[initialCapacity];
+        createThreads();
+    }
+    
+    public void createThreads() {
+        for (int i = 0; i < threads.length; i++) {
+            
+            threads[i] = new Thread(new Runnable() {                
+                               
+                @Override
+                public void run() {
+                    
+                    while (!destroyPool) {
+                        
+                        // Threads 'waiting' in the pool for a task
+                        try {
+                            this.wait();
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(ThreadPool.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+
+                        // Threads begin performing a task once notify has been
+                        // called on them by perform()
+                        boolean hasTask = false;
+                        Task task;
+                        synchronized(taskQueue) {                        
+                            task = taskQueue.poll();
+                            hasTask = true;
+                        }
+                        if (hasTask)
+                            task.run();
+                    }
+                }                
+            }
+            );           
+            
+        }
     }
         
     public int getSize() {
-        return threads.length;
+        int count = 0;
+        for (Thread t : threads) {
+            count++;
+        }           
+        return count;
     }
     
     // Checks the performing array for threads running tasks
     public int getAvaliable() {
         int count = 0;
         for (int i = 0; i < threads.length; i++) {
-            if (performing[i])
+            if (threads[i].getState() == Thread.State.WAITING);
                 count++;
         }
         return count;
@@ -45,11 +87,33 @@ public class ThreadPool {
     
     // Checks the performing 
     public void destroyPool() throws InterruptedException {
-        for (int i = 0; i < threads.length; i++) {
-            if (performing[i]) {
-                threads[i].join();
-                performing[i] = false;
+        
+        // Main thread will spin until all tasks have been completed
+        while (!taskQueue.isEmpty()) {;}
+        
+        // Synchronize on the queue to make sure no tasks get added during
+        // pool destruction
+        synchronized (taskQueue) {
+            destroyPool = true;
+            for (int i = 0; i < threads.length; i++) {
+                if (threads[i].getState() == Thread.State.WAITING) {
+                    threads[i].notify();
+                }
             }
         }
     }
+    
+    public boolean perform(Task task) {
+        taskQueue.add(task);
+        for (int i = 0; i < threads.length; i++) {
+            if (threads[i].getState() == Thread.State.WAITING) {
+                threads[i].notify();
+                break;
+            }
+        }
+        
+        return true;
+    }
 }
+
+
