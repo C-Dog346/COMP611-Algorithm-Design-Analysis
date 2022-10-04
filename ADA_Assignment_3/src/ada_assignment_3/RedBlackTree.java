953f3ef9c24ee5537db5983584cdd59b6757c1ef
Callum Clow <nhs9115@autuni.ac.nz>
2022-10-04 20:53:45 +1300

    rbt rotation attempt

---
+++ ADA_Assignment_3/src/ada_assignment_3/RedBlackTree.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ada_assignment_3;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Callum
+ */
+public class RedBlackTree<E> extends PersistentDynamicTree {
+
+    //Color for a red node.
+    protected static final Color RED = Color.red;
+
+    //Color for a black node.
+    protected static final Color BLACK = Color.black;
+
+    protected class RBTNode extends BinarySearchTree.BinaryTreeNode {
+
+        protected Color color;
+
+        public RBTNode(E element) {
+            super(element);
+            this.color = RED;
+        }
+
+    }
+
+    public void insert(E o) {
+        super.add(o);
+        insertFixup(o);
+    }
+
+    private void insertFixup(E o) {
+        RBTNode y = null;
+        RBTNode x = new RBTNode(o);
+
+        RBTNode root = (RBTNode) this.versionList.get(versionList.size() - 1);
+
+//        while (root.) {
+//
+//        }
+
+    }
+    
+    protected void rightRotate(RBTNode x, RBTNode parent) {
+        RBTNode y = (RBTNode) x.leftChild;
+        
+        x.leftChild = y.rightChild;        
+        y.rightChild = x;
+        
+        if (this.rootNode == x) {
+            this.rootNode = y;
+        }
+        else if (parent.leftChild == x) {
+            parent.leftChild = y;
+        }
+        else {
+            parent.rightChild = y;
+        }
+    }
+    
+    protected void leftRotate(RBTNode x, RBTNode parent) {
+        RBTNode y = (RBTNode) x.rightChild;
+        
+        x.rightChild = y.leftChild;        
+        y.leftChild = x;
+        
+        if (this.rootNode == x) {
+            this.rootNode = y;
+        }
+        else if (parent.rightChild == x) {
+            parent.rightChild = y;
+        }
+        else {
+            parent.leftChild = y;
+        }
+    }
+}
