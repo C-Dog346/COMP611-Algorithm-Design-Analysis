b4e3fa3eea252cc3754479098a406e74a70af24e
Callum Clow <nhs9115@autuni.ac.nz>
2022-10-26 16:05:58 +1300

    assignment 4

---
+++ ADA_Assignment_4/src/ada_assignment_4/BestConversionFinder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ada_assignment_4;
+
+/**
+ *
+ * @author Callum
+ */
+public class BestConversionFinder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
